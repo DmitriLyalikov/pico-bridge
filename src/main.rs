4a2e0ca10dcfbdfd9a25582f209273b8c14be386
DmitriLyalikov <dlyalikov01@manhattan.edu>
2023-01-30 07:05:38 -0500

    removed helper functions from main

--- src/main.rs
+++ src/main.rs
@@ -48,7 +48,7 @@ mod app {
 
     const PIO_CLK_DIV_FRAQ: u8 = 255;
 
-    use crate::setup::Counter;
+    use crate::setup::{Counter, match_usb_serial_buf, write_serial, print_menu};
 
     // USB Device support 
     use usb_device::{class_prelude::*, prelude::*};
@@ -243,9 +243,9 @@ mod app {
                 serial,
                 usb_dev,
 
-                smi_master,
-                smi_tx,
-                smi_rx,
+                smi_master,   // SMI PIO State Machine 
+                smi_tx,       // SMI TX FIFO
+                smi_rx,       // SMI RX FIFO
 
                 counter,
             },
@@ -319,97 +319,5 @@ mod app {
         }
     }
 
-    /* New Tasks */
-
-    // Helper function to ensure all data is written across the serial interface
-    fn write_serial(serial: &mut SerialPort<'static, hal::usb::UsbBus>, buf: &str, block: bool) {
-        let write_ptr = buf.as_bytes();
-
-        // Because the buffer is of constant size and initialized to zero (0) we 
-        //  add a test to determine the size that's really occupied by the str that we
-        // want to send. From index zero to first byte that is as the zero byte value
-        let mut index = 0;
-        while index < write_ptr.len() && write_ptr[index] != 0 {
-            index += 1;
-        }
-        let mut write_ptr = &write_ptr[0..index];
-
-        while !write_ptr.is_empty() {
-            match serial.write(write_ptr) {
-                Ok(len) => write_ptr = &write_ptr[len..],
-                // Meaning the USB write buffer is full
-                Err(UsbError::WouldBlock) => {
-                    if !block {
-                        break;
-                    }
-                }
-                // On error, just drop unwritten data
-                Err(_) => break,
-            }
-        }
-        let _ = serial.flush();
-    }
-
-    // Match the Serial Input commands to a hardware/software request
-    fn match_usb_serial_buf(
-        buf: &[u8; 64],
-        // Add any accessed 'static peripherals (PIO, SPI, etc) that will be controlled by host
-        serial: &mut SerialPort<'static, hal::usb::UsbBus>,
-        counter: &mut Counter,
-    ) {
-        let _buf_len = buf.len();
-        match buf[0] {
-            // Print Menu
-            b'M' | b'm' => {
-                write_serial(serial, "M - Print Menu\n", false);
-                print_menu(serial);
-            }
-            // 0 - Reset counter
-            b'0' => {
-            write_serial(serial, "M - Print Menu\n", false);
-                counter.reset();
-            }
-            // 1 - Increment counter
-            b'1' => {
-                write_serial(serial, "1 - Increment counter\n", false);
-                counter.increment();
-            }
-            // 2 - Start continues counter
-            b'2' => {
-                write_serial(serial, "2 - Start continues counter\n", false);
-                counter.enable(true);
-            }
-            // 3 - Stop continues counter
-            b'3' => {
-                write_serial(serial, "3 - Stop continues counter\n", false);
-                counter.enable(false);
-            }
-            _ => {
-                write_serial(
-                    serial,
-                    unsafe { core::str::from_utf8_unchecked(buf) },
-                    false,
-                );
-                write_serial(serial, "Invalid option!\n", false);
-            }
-        }
-    }
-
-    fn print_menu(serial: &mut SerialPort<'static, hal::usb::UsbBus>){
-        let mut _buf = [0u8; 273];
-        // Create the Menu.
-        let menu_str = "***************** \n\r
-*  RP2040 RPC \n\r
-*  Menu:\n\r
-* \n\r
-*  M / m - Print menu \n\r
-*    0   - smi r phyAddr RegAddr \n\r
-*    1   - smi w phyAddr RegAddr Data \n\r
-*    2   - smi reset \n\r
-*    3   - smi setclk frequency \n\r
-***************** \n\r
-Enter option: ";
-
-        write_serial(serial, menu_str, true);
-    }
+    
 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,4 +1,8 @@
-
+use rp_pico::hal as hal;
+// USB Device support 
+use usb_device::{class_prelude::*, prelude::*};
+// USB Communications Class Device support
+use usbd_serial::SerialPort;
 
 pub struct Counter {
     counter: u32,
@@ -28,5 +32,96 @@ impl Counter {
     pub fn enable(&mut self, state: bool) {
         self.enable = state;
     }
+}
+// Helper function to ensure all data is written across the serial interface
+pub fn write_serial(serial: &mut SerialPort<'static, hal::usb::UsbBus>, buf: &str, block: bool) {
+    let write_ptr = buf.as_bytes();
+
+    // Because the buffer is of constant size and initialized to zero (0) we 
+    //  add a test to determine the size that's really occupied by the str that we
+    // want to send. From index zero to first byte that is as the zero byte value
+    let mut index = 0;
+    while index < write_ptr.len() && write_ptr[index] != 0 {
+        index += 1;
+    }
+    let mut write_ptr = &write_ptr[0..index];
+
+    while !write_ptr.is_empty() {
+        match serial.write(write_ptr) {
+            Ok(len) => write_ptr = &write_ptr[len..],
+            // Meaning the USB write buffer is full
+            Err(UsbError::WouldBlock) => {
+                if !block {
+                    break;
+                }
+            }
+            // On error, just drop unwritten data
+            Err(_) => break,
+        }
+    }
+    let _ = serial.flush();
+}
+
+// Match the Serial Input commands to a hardware/software request
+pub fn match_usb_serial_buf(
+    buf: &[u8; 64],
+    // Add any accessed 'static peripherals (PIO, SPI, etc) that will be controlled by host
+    serial: &mut SerialPort<'static, hal::usb::UsbBus>,
+    counter: &mut Counter,
+) {
+    let _buf_len = buf.len();
+    match buf[0] {
+        // Print Menu
+        b'M' | b'm' => {
+            write_serial(serial, "M - Print Menu\n", false);
+            print_menu(serial);
+        }
+        // 0 - Reset counter
+        b'0' => {
+        write_serial(serial, "M - Print Menu\n", false);
+            counter.reset();
+        }
+        // 1 - Increment counter
+        b'1' => {
+            write_serial(serial, "1 - Increment counter\n", false);
+            counter.increment();
+        }
+        // 2 - Start continues counter
+        b'2' => {
+            write_serial(serial, "2 - Start continues counter\n", false);
+            counter.enable(true);
+        }
+        // 3 - Stop continues counter
+        b'3' => {
+            write_serial(serial, "3 - Stop continues counter\n", false);
+            counter.enable(false);
+        }
+        _ => {
+            write_serial(
+                serial,
+                unsafe { core::str::from_utf8_unchecked(buf) },
+                false,
+            );
+            write_serial(serial, "Invalid option!\n", false);
+        }
+    }
+}
+
+pub fn print_menu(serial: &mut SerialPort<'static, hal::usb::UsbBus>){
+    let mut _buf = [0u8; 273];
+    // Create the Menu.
+    let menu_str = "***************** \n\r
+*  RP2040 RPC \n\r
+*  Menu:\n\r
+* \n\r
+*  M / m - Print menu \n\r
+*    0   - smi r phyAddr RegAddr \n\r
+*    1   - smi w phyAddr RegAddr Data \n\r
+*    2   - smi reset \n\r
+*    3   - smi setclk frequency \n\r
+***************** \n\r
+Enter option: ";
+
+    write_serial(serial, menu_str, true);
 }
 
