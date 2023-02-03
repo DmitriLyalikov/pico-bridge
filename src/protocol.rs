cb17e5a66c4e0032725c1d9b3ba5ead9d5748bc3
DmitriLyalikov <dlyalikov01@manhattan.edu>
2023-02-02 20:20:42 -0500

    implemented send_out and respod traits for HostRequest and SlaveResponse

--- src/protocol.rs
+++ src/protocol.rs
@@ -2,17 +2,36 @@
     use core::{marker::PhantomData};
     use core::result::Result;
     use core::{mem, slice};
-
-    use self::Slave::{SlaveResponse, NotReady, SlaveErr};
+    use self::Slave::{SlaveResponse, NotReady, SlaveErr, HostErr};
 
     pub trait Send{
-        fn send_out(&self) -> Result<SlaveResponse<NotReady>, SlaveErr> {
-            Err(SlaveErr::Timeout)
+        fn send_out(&mut self) -> Result<SlaveResponse<NotReady>, SlaveErr> {
+            // Match on the device facing interface and send payload to its TX FIFO
+            // Return the constructed SlaveResponse
+            Ok(SlaveResponse::new())
         }
+    }
+
+    pub trait Respond {
+        fn respond_to_host(&self) -> HostErr {
+            // Match on host_interface and send payload back on that channel
+            // This needs to be done on a task that has access to all host facing interfaces
+            HostErr::None
+        }
+    }
+
+    #[derive(Copy, Clone)]
+    pub enum ValidHostInterfaces {
+        Serial = 0b00,
+        UART = 0b01,
+        SPI = 0b10,
+        None = 0b11,
     }
 
 pub mod Host {
     use core::{marker::PhantomData};
+    use super::Send;
+    use super::{SlaveResponse, ValidHostInterfaces};
 
     // State of the request
     pub trait State {}
@@ -50,6 +69,7 @@ pub mod Host {
         state: PhantomData<S>,
         proc_id: u8,
         interface: ValidInterfaces,
+        host_config: ValidHostInterfaces,
         operation: ValidOps,
         checksum: u8,         // Wrapping checksum
         size: u8,             // A value between 0 and 4
@@ -63,21 +83,44 @@ pub mod Host {
                 state: PhantomData,
                 proc_id: self.proc_id,
                 interface: self.interface,
+                host_config: self.host_config,
                 operation: self.operation,
                 checksum: self.checksum,
                 size: self.size,       
                 payload: self.payload,
             }
         }
-
     }
     
+    impl Send for HostRequest<Clean> {
+        fn send_out(&mut self) -> Result<super::Slave::SlaveResponse<super::Slave::NotReady>, super::Slave::SlaveErr> {
+            match self.interface {
+                ValidInterfaces::Config => {
+                    // SysConfig_Handler::Spawn(self.operation, self.size, self.payload)
+                }
+                ValidInterfaces::SMI => {
+                    // SMI_Handler::Spawn(self.operation, self.size, self.payload)
+                }
+                ValidInterfaces::JTAG => {
+                    // JTAG_Handler::Spawn(self.operation, self.payload)
+                }
+                _ => {
+                    // Do something 
+                }
+            }
+            let mut SR = SlaveResponse::new();
+            SR.set_host_config( self.host_config);
+            SR.set_proc_id(self.proc_id);
+            Ok(SR)
+        }
+    }
     impl HostRequest<Unclean> {
         pub fn new() -> HostRequest<Unclean> {
             HostRequest {
                 state: PhantomData,
                 proc_id: 0_u8,
                 interface: ValidInterfaces::None,
+                host_config: ValidHostInterfaces::None,
                 operation: ValidOps::None,
                 checksum: 0_u8,
                 size: 0_u8,           
@@ -122,6 +165,7 @@ pub mod Host {
 
 pub mod Slave {
     use core::{marker::PhantomData};
+    use super::{Respond, ValidHostInterfaces};
 
         // State of the request
     pub trait State {}
@@ -137,11 +181,6 @@ pub mod Slave {
     impl State for NotReady {}
     impl State for Ready {}
 
-    pub enum ValidHostInterfaces {
-        Serial = 0b00,
-        UART = 0b01,
-        SPI = 0b10,
-    }
     pub struct SlaveResponse<S: State> {
         state: PhantomData<S>,
         proc_id: u8,
@@ -151,8 +190,82 @@ pub mod Slave {
 
     }
 
+    impl <S: State> SlaveResponse<S>{
+        fn transition<To: State>(self, _: To) -> SlaveResponse<To> {
+            SlaveResponse {
+                state: PhantomData,
+                proc_id: self.proc_id,
+                host_config: self.host_config,
+                size: self.size,       
+                payload: self.payload,
+            }
+        }
+    }
+
+    impl Respond for SlaveResponse<Ready> {
+        fn respond_to_host(&self) -> HostErr {
+            match self.host_config {
+                ValidHostInterfaces::Serial => {
+                    // Serial_Respond_Task.spawn(size, payload)
+                    HostErr::None
+                }
+                ValidHostInterfaces::UART => {
+                    // UART_Respond_Task.spawn(size, payload)
+                    HostErr::None
+                }
+                ValidHostInterfaces::SPI => {
+                    // SPI_Respond_Task.spawn(size, payload)
+                    HostErr::None
+                }
+                _ => {
+                    // Should never happen
+                    HostErr::Timeout
+                }
+            }
+        }
+    }
+
+    impl SlaveResponse<NotReady> {
+        pub fn new() -> SlaveResponse<NotReady> {
+            SlaveResponse { 
+                state: PhantomData,
+                proc_id: 0_u8,
+                host_config: ValidHostInterfaces::None,
+                size: 0_u8,       
+                payload: [0_u8; 4],
+            }
+        }
+
+        pub fn set_proc_id(&mut self, proc_id: u8) {
+            self.proc_id = proc_id;
+        }
+
+        pub fn set_host_config(&mut self, interface: ValidHostInterfaces) {
+            self.host_config = interface;
+        }
+
+        pub fn set_size(&mut self, size: u8) {
+            self.size = size;
+        }
+
+        pub fn set_payload(&mut self, payload: [u8; 4]) {
+            self.payload = payload;
+        }
+
+        pub fn init_ready(mut self) -> SlaveResponse<Ready> {
+            // if let valid_packet = checksum(self.checksum) ...
+            // Any other kind of packet sanitizing
+            self.transition(Ready {__private: () })
+        }
+    }
     pub enum SlaveErr {
         Timeout,
+        None,
+    }
+
+    pub enum HostErr {
+        Timeout,
+        Overflow,
         None,
     }
     pub enum SlaveCode {
